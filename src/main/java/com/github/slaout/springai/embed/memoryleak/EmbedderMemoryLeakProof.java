e13f6bf48c70f9e6ac70eadc0a1853f823b350b2
Sébastien Laoût <slaout@users.noreply.github.com>
2024-09-27 22:06:14 +0200

    feat: simplified application

--- src/main/java/com/github/slaout/springai/embed/memoryleak/EmbedderMemoryLeakProof.java
+++ src/main/java/com/github/slaout/springai/embed/memoryleak/EmbedderMemoryLeakProof.java
@@ -4,7 +4,6 @@ import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,43 +14,25 @@ import static com.github.slaout.springai.embed.memoryleak.RandomWordsGenerator.g
 @SpringBootApplication
 public class EmbedderMemoryLeakProof implements CommandLineRunner {
 
-    private final ApplicationContext applicationContext;
     private final EmbeddingModel embeddingModel;
 
-    public EmbedderMemoryLeakProof(
-            ApplicationContext applicationContext,
-            EmbeddingModel embeddingModel
-    ) {
-        this.applicationContext = applicationContext;
+    public EmbedderMemoryLeakProof(EmbeddingModel embeddingModel) {
         this.embeddingModel = embeddingModel;
     }
 
     // Run with: -Xms1g -Xmx1g -XX:NativeMemoryTracking=summary
     public static void main(String[] args) {
-        System.setProperty("ai.djl.pytorch.num_interop_threads", "4");
-        System.setProperty("ai.djl.pytorch.num_threads", "4");
-
         SpringApplication.run(EmbedderMemoryLeakProof.class, args);
     }
 
     @Override
-    public void run(String... args) throws InterruptedException {
+    public void run(String... args) {
         int embeddingThreads = 4; // Same with 1, but slower
-        Thread[] threads = new Thread[embeddingThreads + 1];
-
         for (int i = 0; i < embeddingThreads; i++) {
-            threads[i] = new Thread(this::embedALot);
-            threads[i].start();
+            new Thread(this::embedALot).start();
         }
 
-        threads[threads.length - 1] = new Thread(this::showRamUsageEverySecond);
-        threads[threads.length - 1].start();
-
-        for (Thread thread : threads) {
-            thread.join();
-        }
-
-        SpringApplication.exit(applicationContext, () -> 0);
+        new Thread(this::showRamUsageEverySecond).start();
     }
 
     private void embedALot() {
